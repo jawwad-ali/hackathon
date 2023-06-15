e38de86565955cd43ed28ef79e3b3ef99aa4620c
jawwad-ali <alijawwad001@gmail.com>
2023-06-15 15:36:25 +0500

    remove unused importx2

--- app/components/CartData.tsx
+++ app/components/CartData.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, Suspense } from "react";
+import React, { useEffect, useState } from "react";
 
 import { client } from "../../sanity/lib/client";
 
